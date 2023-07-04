966cd11e67547dece33c3c51a4926e604e94ffb0
Juan Pablo Cardenas <juan.cardenas.r@usach.cl>
2023-07-03 22:54:28 -0400

    añadida 'idea' sobre el menú de opciones (el nuevo archivo class system es un solo un modelo inicial)

---
+++ lab3_21209320_CardenasRueda/src/lab3_21209320_CardenasRueda/FileSystem_21209320_CardenasRueda.java
@@ -0,0 +1,17 @@
+package lab3_21209320_CardenasRueda;
+
+public class FileSystem_21209320_CardenasRueda implements FileSystem_interfaz_21209320_CardenasRueda{
+
+	String nombre;
+	
+	@Override
+	public void crearSistema(String nombre) {
+		
+	}
+
+	@Override
+	public void getSistema() {
+		
+	}
+	
+}
--- lab3_21209320_CardenasRueda/src/lab3_21209320_CardenasRueda/Main.java
+++ lab3_21209320_CardenasRueda/src/lab3_21209320_CardenasRueda/Main.java
@@ -1,10 +1,32 @@
 package lab3_21209320_CardenasRueda;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("## Bienvenido al menú del sistema ##");
+		System.out.println();
+		System.out.println("Selecciona tu opción indicando únicamente el número:");
+		System.out.println();
+		System.out.println("1.- Crear un sistema");
+		System.out.println("2.- Editar el sistema");
+		System.out.println("3.- Imprimir el sistema");
+		System.out.println();
+		System.out.println("Escoja la opción: ");
+		
+		int opcion = scanner.nextInt();
+		
+		switch(opcion) { //Se tiene pensado hacer switch para las distintas opciones
+		case 1:
+			break;
+		}
+		
+		scanner.close();
+		
 	}
 
 }
